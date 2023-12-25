a86ee4e7a755939d671ef78c22d0d405a08d32a3
ASJ978 <154775123+ASJ978@users.noreply.github.com>
2023-12-25 04:56:36 +0000

    Added input for name, age, how to help, and a list of options.

---
+++ main.py
@@ -0,0 +1,13 @@
+## All dialogue is temporary, will be tweaked later
+
+##Greet User
+print("Hello, I am your chat bot assisstant")
+##Ask for name and age
+name = input("What is your name? \n")
+age = input("What is your age? \n")
+##Ask how to help
+input(f'How can I help you {name}? \n')
+##List of options on how to continue
+choice = input("You can choose: \n 1. placeholder1 \n 2. placeholder2 \n 3. placeholder3 \n 4. placeholder4 \n 5. exit \n")
+if choice == "5":
+    print("Goodbye")
