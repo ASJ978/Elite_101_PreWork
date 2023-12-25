4e1fb01ef8c18e864e93caadd511fd856bbe7dd4
ASJ978 <154775123+ASJ978@users.noreply.github.com>
2023-12-25 05:09:43 +0000

    added more details to dialogue and exit program at the end of the if-statement.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 ## All dialogue is temporary, will be tweaked later
 
 ##Greet User
-print("Hello, I am your chat bot assisstant")
+print("Hello, I am your chat bot assisstant!")
 ##Ask for name and age
+
 name = input("What is your name? \n")
 age = input("What is your age? \n")
+
 ##Ask how to help
-input(f'How can I help you {name}? \n')
+
+input(f'You are {age}? I have a friend who is {age}! How can I help you {name}? \n')
+
 ##List of options on how to continue
-choice = input("You can choose: \n 1. placeholder1 \n 2. placeholder2 \n 3. placeholder3 \n 4. placeholder4 \n 5. exit \n")
+
+choice = input("\nYou can choose: \n 1. placeholder1 \n 2. placeholder2 \n 3. placeholder3 \n 4. placeholder4 \n 5. exit \n")
+
 if choice == "5":
-    print("Goodbye")
+  print(f'Goodbye {name}! See you next time!')
+  exit
